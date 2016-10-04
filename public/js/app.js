5556c3d6924d396e3605122317130d0fc875b918
Melinda Nel <melinda@corpcam.com>
2016-10-04 10:19:53 +0200

    Add name support

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,8 @@
 var socket = io();
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
+
+console.log(name + ' joined ' + room);
 
 socket.on('connect', function () {
 	console.log('Connected to Socket IO Server');
@@ -6,10 +10,14 @@ socket.on('connect', function () {
 
 socket.on('message', function (message) {
 	var momentTimestamp = moment.utc(message.timestamp);
+	var $message = jQuery('.messages');
+
 	console.log('New Message: ');
 	console.log(message.text);
 
-	jQuery('.messages').append('<p><strong>' + momentTimestamp.local().format('h:mm a') + ':</strong> ' + message.text + '</p>'); //. = class # = id
+	$message.append('<div style=\'background-color:#FF0000;\'><p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p></div>')
+	$message.append('<p>' + message.text + '</p>');
+	//$message.append('<p><strong>' + momentTimestamp.local().format('h:mm a') + ':</strong> ' + message.text + '</p>'); //. = class # = id
 
 });
 
@@ -20,6 +28,7 @@ $form.on('submit', function (event) {
 	$message = $form.find('input[name=message]');
 	event.preventDefault(); //dont send old fasioned way with postbacks
 	socket.emit('message', {
+		name: name,
 		text: $message.val()
 	});
 	$message.val('');
